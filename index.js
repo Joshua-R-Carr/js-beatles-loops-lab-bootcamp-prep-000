20d45cedc86cb60b830a1c2c3ef7dfd596188c5a
josh.ray.carr <josh.ray.carr@gmail.com>
2019-03-30 07:25:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(array1, array2){
     var sol = [];
     for(i = 0, len = array1.length; i < len; i++){
         a = String(array1[i]) + " plays " + String(array2[i]);
-        (sol.push(a);
+        sol.push(a);
     }
     return sol;
 }
